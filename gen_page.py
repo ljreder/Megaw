2010bb5bf6ef5faedabd2d3becf6a25fe85efc1c
Leonard J. Reder <reder@ieee.org>
2023-10-10 01:00:15 -0700

    Final initial version of page.

--- gen_page.py
+++ gen_page.py
@@ -2,6 +2,7 @@
     Python to product an index.html page of Megaw legacy materials.
 """
 import os
+import datetime
 
 PAGE_FILENAME = "index.html"
 TOP_DIR       = "Programs"
@@ -12,6 +13,49 @@ BEGIN = """<!DOCTYPE html>
         <title>Megaw</title>
     </head>
     <body>
+        <header>
+            <div style="text-align:center;">
+                <h1>The Megaw Theatre List of Programs (1974-1986)</h1>
+            </div>
+        </header>
+        <p>
+         This page contains a partial collection of the original programs created to support The Megaw Theatre
+         shows.  The Megaw theatre was a 99 seat equity weaver theatre that was open from 1974 to 1987
+         and located in Northridge, CA.  After the Megaw closed it's doors in California the founders 
+         (Sydney May Morrison and Elaine EE Moe) reinvented the theatre as <a href="https://megawtheatreinc.com/">The Megaw Theatre and Acting Studio, Inc.</a> relocating to
+         Scottsdale, AZ. For the unfamilliar, a theatre that has only 99 seats can be exempt from paying
+         union actors. Thus the Megaw theatre was an all volunteer effort but still had professionals
+         performing (without pay) in the productions. This page is a temporary location for the partial
+         collection of programs.  The programs where graciously supplied by JoAnn Iaccino (formally JoAnna
+         Mashall). JoAnn designed the original Megaw logo and many of the programs. The programs where
+         scanned and placed on this web page by Leonard (Lenny) Reder.  Lenny spent a couple years at
+         Megaw as a young man assisting with and learning all about technical theatre (e.g. set construction,
+         stage lighting, etc.).<br><br>
+
+         Megaw Theatre opened it's doors on September 27, 1974 with the production
+         of THE LITTLE FOXES. This was almost 50 years ago.  It is my hope to grow
+         this web page with production photographs and other information into a 50th.
+         anniversary Megaw Theatre legacy page. The goal of the page is to preserve the
+         memory for the many people that worked at Megaw Theatre.<br><br>
+
+         Finally I would be remiss if I did not mension two people I personnally and foundly
+         remember who have passed away and are no longer with us - Sydney Morrison and Dave Lukas.
+         Sydney May Morrison (Syd) the executive producer and founder of Megaw.  Syd one summer
+         day in 1974 interviewed me (a junior in high school) to particapate at the Megaw as a
+         technical theatre apprentice which was the beginning of a couple years of learning.  Syd 
+         was my advocate at Megaw and taught me about work.  Dave Lukas, an extrodonary
+         theatrical set designer and scenic carpetar, would be my first true mentor. I have
+         life long memories of Dave and myself in 1976 working almost daily hammering out set
+         after set at that little theatre in Northridge. For the uninitiated you will notice
+         these two individuals names along with Elaine (EE) Moe in every single program listed.
+         EE was that other Megaw Theatre founder that was the ultimate expert on everything
+         theatrical and is still actively teaching acting. With certainty everyone
+         misses Syd and Dave who knew and worked with them at Megaw.<br><br>
+         
+         Leonard J. Reder<br>
+         Email: <a href = "mailto: reder@ieee.org">reder@ieee.org</a><br>
+         Last Updated: $DATE$<br>
+         </p>
         <h2>Programs (in Alphabetic order not chronological):</h2>
         <ol>
 """
@@ -60,6 +104,10 @@ def main():
     """
     global PAGE_FILENAME
     global TOP_DIR
+    # get today's date
+    today = datetime.date.today()
+    today = today.strftime("%B %d, %Y")
+    print(today)
     # get list of sub-directories
     dir_name_list = os.listdir(TOP_DIR)
     # get list of file names
@@ -68,9 +116,9 @@ def main():
     # open page file for writing
     #
     with open(PAGE_FILENAME, 'w') as f:
-        
-        f.write(BEGIN)
-        
+        b = BEGIN.replace("$DATE$",today)
+        f.write(b)
+
         i = 0
         for d in dir_name_list:
             filename = file_list[i]
